5187ee4e559e6552263cde69baca98a23c965e1e
chiiglaaa <99479509+chiiglaaa@users.noreply.github.com>
2023-01-28 01:21:22 +0400

    Update main.py

--- Codewars/8 kyu/Is the string uppercase?/main.py
+++ Codewars/8 kyu/Is the string uppercase?/main.py
@@ -1,10 +1,8 @@
 def is_uppercase(inp):
     if len(inp) <= 1 and inp[0].islower():
-        print(inp)
         return False
     elif len(inp) > 1:
         for i in range(len(inp)):
-            print(i)
             if inp[i].islower():
                 return False
     return True
